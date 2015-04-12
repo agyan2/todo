8391cb9fad9f5e3c0632e73ec9fb150b793c78af
agyan2 <abhinav0508@gmail.com>
2015-04-11 22:50:58 -0700

    2nd commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
+    var mongoose = require('mongoose');
 
 // configuration =================
 
